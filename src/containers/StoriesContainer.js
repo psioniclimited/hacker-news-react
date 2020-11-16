7a90a9df4451209257f3d93a4107d932dde51dd1
sabbir-anwar <protik26@gmail.com>
2020-11-16 18:01:03 +0600

    getStory method created

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getStoryIds } from "../services/hnApi";
+import { getStoryIds, getStory } from "../services/hnApi";
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
